b6885ac26fe1e79f5793bf22c9cdea7b558ec4bb
GercKLIM <gerceg0808@yandex.ru>
2024-04-09 18:03:32 +0300

    - Добавил свои версии прогонки и решения случая 1 - Улучшил визуализацию - Добавил тест варианта 5 из вариантов группы - В класс PDE_data добавлены новые параметры

--- Programms/Code/SolvePDE.cpp
+++ Programms/Code/SolvePDE.cpp
@@ -55,6 +55,7 @@ std::vector<DT> TridiagonalMatrixAlgorithm(
  *            u0 - initial temperature
  * return:    initial state of the system
  * */
+
 std::vector<double> init_state(int n, double u0)
 {
     std::vector<double> result(n, u0);
@@ -81,6 +82,7 @@ double w(double a, double u_i, double u_im, double h) {
     return a * (u_i-u_im)/h;
 }
 
+// То что написал Ваня
 bool ExplicitScheme(double tau, double h, double sigma, PDE_data test, std::string filename="ExpScheme"){
     double c = test.c;
     double rho = test.rho;
@@ -122,7 +124,7 @@ bool ExplicitScheme(double tau, double h, double sigma, PDE_data test, std::stri
         As[num_space_steps-1] = 0;
         Cs[num_space_steps-1] = 1;
         Fs[num_space_steps-1] = u_0;
-        for(int j = 0; j<num_time_steps; ++j) {
+        for(int j = 0; j < num_time_steps; ++j) {
             t_i += tau;
             for (int i = 1; i < num_space_steps - 1; ++i) {
                 x_i += h;
@@ -142,12 +144,122 @@ bool ExplicitScheme(double tau, double h, double sigma, PDE_data test, std::stri
         fpoints.close();
         return true;
     }
-    else
+    else {
         std::cout << "log[ERROR]: Couldn't open or create a file" << std::endl;
-    return false;
+        return false;
+    }
 };
 
-/* Функция для решения PDE в случае 1 (по методичке) */
-void SolvePDE_1(PDE_data test, double h, double tau, double sigma, std::string filename){
 
+
+
+// Моя переделка функций Вани:
+
+
+std::vector<double> progonka(std::vector<double>& a, std::vector<double>& b, std::vector<double>& c, std::vector<double>& d) {
+
+    int n = b.size();
+    std::vector<double> alph(n, 0);
+    std::vector<double> beth(n, 0);
+    std::vector<double> solve(n, 0);
+    double tmp;
+
+    //vectinput();
+    alph[0] = 0;
+    beth[0] = 0;
+    tmp = b[0];
+    alph[1] = c[0] / tmp;
+    beth[1] = d[0] / tmp;
+
+    //std::cout << alph[1] << " " << beth[1] << "\n";
+    for (int i = 1; i < n - 1; i++) {
+        tmp = b[i] - a[i] * alph[i];
+        alph[i + 1] = c[i] / tmp;//(b[i] - a[i] * alph[i]);
+        beth[i + 1] = (d[i] + a[i] * beth[i]) / tmp; // (b[i] - a[i] * alph[i]);
+    }
+
+    solve[n - 1] = (d[n - 1] + a[n - 1] * beth[n - 1]) / (b[n - 1] - a[n - 1] * alph[n - 1]);
+
+    for (int i = n - 2; i >= 0; i--) {
+        solve[i] = alph[i + 1] * solve[i + 1] + beth[i + 1];
+    }
+
+    return solve;
+}
+
+/* Функция для решения PDE в случае 1 (по методичке) */
+bool SolvePDE_1(PDE_data test, double tau, double h, double sigma, std::string filename){
+
+
+    double t_0 = 0; // Начальное время
+    double x_0 = 0; // Начальное условие?
+
+    // Количество шагов по времени и пространству
+    int num_time_steps = static_cast<int>((test.T-t_0) / tau);
+    int num_space_steps = static_cast<int>((test.L - x_0) / h);
+
+    // TODO: брать граничное условие из теста (здесь начальная температура)
+    double u_0 = 10;
+
+    // Инициализация начального состояния
+    std::vector<double> state_0 = init_state(num_space_steps, u_0); //TODO: расширить init_state
+    std::vector<double> As(num_space_steps, 0);
+    std::vector<double> Cs(num_space_steps, 0);
+    std::vector<double> Bs(num_space_steps, 0);
+    std::vector<double> Fs(num_space_steps, 0);
+
+    std::string path = "./OutputData/" + filename + ".txt";
+    std::ofstream fpoints(path);
+    std::cout << "log[INFO]: Starting ExplicitScheme" << std::endl;
+    std::cout << "log[INFO]: Opening a file \"" << filename << "\" to write..." << std::endl;
+
+    if (fpoints.is_open()) {
+
+        double t_i = t_0;
+        std::vector<double> state_i = state_0;
+        int ind = 0;
+        writeVectorToFile(fpoints, t_i, state_i);
+        double x_i = x_0;
+        Cs[0] = 1;
+        Bs[0] = 0;
+        As[0] = 0;
+        Fs[0] = u_0;
+        Bs[num_space_steps-1] = 0;
+        As[num_space_steps-1] = 0;
+        Cs[num_space_steps-1] = 1;
+        Fs[num_space_steps-1] = u_0;
+
+        for (int j = 0; j < num_time_steps; ++j) {
+            t_i += tau;
+
+            for (int i = 1; i < num_space_steps - 1; ++i) {
+                x_i += h;
+
+                //double a_i = a(test.K_ptr, x_i, x_i - h);
+                double a_i = 0.5 * (test.K(x_i) + test.K(x_i - h));
+
+                //double a_ip = a(test.K_ptr, x_i + h, x_i);
+                double a_ip = 0.5 * (test.K(x_i + h) + test.K(x_i - h));
+
+                As[i] = sigma / h * a_i;
+                Bs[i] = sigma / h * a_ip;
+                Cs[i] = As[i] + Bs[i] + test.c * test.rho * h / tau;
+                Fs[i] = test.c * test.rho * h / tau * state_i[i]
+                        + (1 - sigma) * (w(a_ip, state_i[i + 1],
+                                           state_i[i], h) - w(a_i, state_i[i], state_i[i - 1], h));
+            }
+
+            Cs = (-1.) * Cs;
+            Fs = (-1.) * Fs;
+            state_i = progonka(As, Cs, Bs, Fs);
+            writeVectorToFile(fpoints, t_i, state_i);
+        }
+
+        fpoints.close();
+        return true;
+
+    } else {
+        std::cout << "log[ERROR]: Couldn't open or create a file" << std::endl;
+        return false;
+    }
 }
--- Programms/Code/main.cpp
+++ Programms/Code/main.cpp
@@ -6,7 +6,7 @@
 //
 
 #include <iostream>
-#include "algebra.h"  // Алгебра векторов и матриц
+#include "algebra.h"    // Алгебра векторов и матриц
 #include "TESTS.cpp"    // Класс условий тестов
 #include "SolvePDE.cpp" // Методы решения PDE
 
@@ -28,12 +28,46 @@ int main() {
     test1.set_K([](double x) { return x; });
     test1.set_G_left([](double x) { return x; });
     test1.set_G_right([](double x) { return x; });
-    test1.show(); // Вывод информации о тесте
-    //std::cout << test1.info();
-    //std::cout << "K(2) = " << test1.K(2) << std::endl;
+    //test1.show(); // Вывод информации о тесте
+
+    // Тест: Вариант 5
+    PDE_data test5;
+    test5.c = 2;
+    test5.rho = 0.25;
+    test5.L = 1;
+    test5.t0 = 0.5;
+    test5.T = 10;
+    test5.u0 = 0.2;
+    test5.set_K([](double x) {
+
+        double x1 = 0.5, x2 = 2./3.;
+        double k1 = 2. , k2 = 0.5;
+        double L = 1;
+
+        if (x <= x1) {
+            return k1;
+
+        } else if (x < x2) {
+            return (k1 * ((x - x2) / (x1 - x2)) + k2 * ((x - x1) / (x2 - x1)));
+
+        } else if (x <= L) {
+            return k2;
+
+        } else {
+            return 0.;
+
+        }
+    });
+
+    test5.set_G_left([](double x) { return 0.2; });
+    test5.set_G_right([](double x) { return 10; });
+
+    /* Случай 1. */
+    //ExplicitScheme(2, 1, 1, test1);
+    SolvePDE_1(test5, 0.01, 0.01, 0.1, "ExpScheme_test5");
+
+
+
 
     std::cout << std::endl << "Complete!" << std::endl;
-
-    ExplicitScheme(2, 1, 1, test1);
 }
-
